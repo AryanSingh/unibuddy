6454a9b6ddcbe2a79b8377c6b8633d35d0a139b8
aryan <aryan.iitgn@gmail.com>
2020-05-29 12:42:20 +0530

    implementing search and connecting to frontend

--- src/App.js
+++ src/App.js
@@ -7,24 +7,48 @@ import HomePage from './components/HomePage';
 
 export class App extends Component {
 
-	componentDidMount(){
-		this.hashCreate()
+	constructor(){
+		super();
+		this.state = {
+			currentSearch: '',
+			table: {},
+			searchResults: []
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState){
+		if(prevState.currentSearch !== this.state.currentSearch){
+			console.log('currentSearch', this.state.currentSearch)
+		}
 
 	}
+
+	componentDidMount(){
+		this.hashCreate()
+	}
+
+	setCurrentSearch = (search) => {
+		this.setState({ currentSearch: search })
+	};
+
+	doSearch = () => {
+		
+	};
 
 	hashCreate = () => {
 		let table = {};
 		data.summaries.map((summary) => {
 			table = createHash(table, summary)
 		});
+		this.setState({ table: table });
 		console.log('table', table)
 	};
 
 
   render(){
 		return (
-      <div className="App">
-        <HomePage/>
+      <div className="App" >
+        <HomePage setCurrentSearch={this.setCurrentSearch} hashTable={this.state.table} />
       </div>
 		);
   }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import { TiMessages } from 'react-icons/ti';
+import { BsSearch } from 'react-icons/bs';
 import { IconContext } from "react-icons";
 
 
@@ -22,6 +23,29 @@ export class HomePage extends Component {
 
 	}
 
+	renderList = () => {
+		return(
+			<ListWrapper>
+				{this.state.list.map((item) => {
+					return(
+						<ListItem key={item}>
+							{item}
+						</ListItem>
+						)
+				})}
+			</ListWrapper>
+		)
+	};
+
+	calculateResults = () => {
+
+	};
+
+	handleChange = (value) => {
+		console.log('value change', value);
+		this.props.setCurrentSearch(value)
+	};
+
 	render(){
 		return (
 			<HomeContainer>
@@ -29,13 +53,21 @@ export class HomePage extends Component {
 					<Header>
 						<IconContainer>
 							<IconContext.Provider value={{ color: "#FFFFFF", className: "global-class-name", size: 40 }}>
-
 							<TiMessages />
 							</IconContext.Provider>
 						</IconContainer>
 						<HeaderTextWrapper>
 							<HeaderText>Unibuddy</HeaderText>
 						</HeaderTextWrapper>
+						<InputContainer>
+							<SearchIcon>
+							<IconContext.Provider value={{ color: "#000000", className: "global-class-name", size: 40 }}>
+								<BsSearch/>
+							</IconContext.Provider>
+							</SearchIcon>
+							<Input onChange={(event) => this.handleChange(event.target.value)}/>
+							{this.renderList()}
+						</InputContainer>
 					</Header>
 				</Wrapper>
 			</HomeContainer>
@@ -68,24 +100,65 @@ const Header = styled.div`
 	height: 100px;
 	width: 100%;
 	background-color: #000000;
-	opacity: .2;
 	display: flex;
 	flex-direction: row;
 	align-items: center;
+	text-transform: uppercase;
+	position: relative;
 `;
 
 const IconContainer = styled(flexDefault)`
+	margin-left: 10px;
 	
 `;
 
 const HeaderTextWrapper = styled(flexDefault)`
-	
-	
+	margin-left: 10px;
 `;
 
 const HeaderText = styled.p`
 	font-family: Roboto;
 	font-size: 24px;
-	font-weight: 400;
+	font-weight: 500;
 	color: #FFFFFF;
+	letter-spacing: .1rem;
+`;
+
+
+const InputContainer = styled(flexDefault)`
+	position: relative;
+	left: 50%;
+  transform: translate(-50%, 0);
+`;
+
+const Input = styled.input`
+	width: 40vw;
+	height: 60px;
+	font-family: Roboto;
+	font-size: 20px;
+	font-weight: 400;
+	padding-left: 60px;
+	color: #000000;
+	background-color: #FFFFFF;
+	opacity: 1;
+	
+`;
+
+const SearchIcon = styled.div`
+	position: absolute;
+	margin-left: 10px;
+	left:0;
+`;
+
+const ListItem = styled(flexDefault)`
+	padding: 0 0 0 60px;
+  width: 40vw;
+`;
+
+const ListWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	background: #FFFFFF;
+	position: absolute;
+	top: 65px;
 `;
