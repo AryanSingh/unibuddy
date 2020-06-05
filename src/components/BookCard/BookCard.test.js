b69cf78ae7bc2a0182b18ed697794a25aa876a53
aryan <aryan.iitgn@gmail.com>
2020-06-05 13:40:39 +0530

    book card failing test passed, rendering successfully

--- src/components/BookCard/BookCard.test.js
+++ src/components/BookCard/BookCard.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BookCard from './BookCard';
+import PropTypes from 'prop-types';
+
 
 
 it("renders without crashing", () => {
 	const div = document.createElement("div");
-	ReactDOM.render(<BookCard/>, div);
+	ReactDOM.render(
+		<BookCard index={1} book={{id: 1, title: 'test', summary: 'test', author: 'test'}} deleteBook={() => {}}/>, div);
 });
+
+
+// BookCard.propTypes = {
+// 	index: PropTypes.number,
+// 	book: PropTypes.shape({
+// 		id: PropTypes.number,
+// 		title: PropTypes.string,
+// 		summary: PropTypes.string,
+// 		author: PropTypes.string
+// 	}),
+// 	deleteBook: PropTypes.func
+// };
